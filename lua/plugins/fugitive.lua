0a6d189588d5f032dc66d3c3cefead76af1ed6b9
Martin Krauskopf <martin.krauskopf@gmail.com>
2025-03-10 12:07:00 +0100

    vim-fugitive: mappings for Git status and diff

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,6 +4,8 @@ return {
 
     config = function()
       vim.keymap.set("n", "<leader>ga", ":Git blame<CR>", { desc = "Git blame" })
+      vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>", { desc = "Git diff" })
+      vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "Git status" })
     end,
   },
   {
