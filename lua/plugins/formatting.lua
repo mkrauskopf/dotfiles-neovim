b7d3095fb0545c4768934a4d84c7c4f4cb963d84
Martin Krauskopf <martin.krauskopf@gmail.com>
2025-03-11 17:24:11 +0100

    Provide `FormatToggle` to allow to disable auto-formatting
    See:
    
    - https://github.com/stevearc/conform.nvim/issues/39
    - https://github.com/stevearc/conform.nvim/issues/192

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,6 +3,7 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local conform = require("conform")
+    local notify = require("notify")
 
     conform.setup({
       formatters_by_ft = {
@@ -23,10 +24,43 @@ return {
         graphql = { "prettier" },
         liquid = { "prettier" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        timeout_ms = 500,
-      },
+
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          lsp_fallback = true,
+          timeout_ms = 500,
+        }
+      end,
+    })
+
+    local function show_notification(message)
+      notify(message, "info", { title = "conform.nvim" })
+    end
+
+    vim.api.nvim_create_user_command("FormatToggle", function(args)
+      local is_global = not args.bang
+      if is_global then
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+        if vim.g.disable_autoformat then
+          show_notification("Autoformat-on-save disabled globally")
+        else
+          show_notification("Autoformat-on-save enabled globally")
+        end
+      else
+        vim.b.disable_autoformat = not vim.b.disable_autoformat
+        if vim.b.disable_autoformat then
+          show_notification("Autoformat-on-save disabled for this buffer")
+        else
+          show_notification("Autoformat-on-save enabled for this buffer")
+        end
+      end
+    end, {
+      desc = "Toggle autoformat-on-save",
+      bang = true,
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
@@ -37,4 +71,3 @@ return {
     end, { desc = "Format file or range (in visual mode)" })
   end,
 }
-
