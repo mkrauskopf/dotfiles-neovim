610f82bb1ba28cd35268aa6499a79ab47b66d727
Martin Krauskopf <martin.krauskopf@gmail.com>
2025-03-14 14:40:18 +0100

    Mapping for fugitive "GBrowse"

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       vim.keymap.set("n", "<leader>ga", ":Git blame<CR>", { desc = "Git blame" })
       vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>", { desc = "Git diff" })
+      vim.keymap.set("n", "<leader>go", ":GBrowse<CR>", { desc = "Open (remote) file in browse" })
       vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "Git status" })
     end,
   },
