e6f91cb44a1d61baf24c40e5dcf7f5cfa9e0fa5f
Martin Krauskopf <martin.krauskopf@gmail.com>
2025-03-19 19:35:04 +0100

    Simplify Telescope mapping configuration with `keys` function

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,47 +14,61 @@ return {
     })
 
     require("telescope").load_extension("fzf")
+  end,
+
+  keys = function()
     local builtin = require("telescope.builtin")
 
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help" })
-    vim.keymap.set("n", "<leader>gr", builtin.live_grep, { desc = "Live Grep" })
-    vim.keymap.set("n", "<leader>gc", builtin.grep_string, { desc = "Grep string under the Cursor" })
-    vim.keymap.set("n", "<leader>ef", builtin.find_files, { desc = "Edit file (telescope)" })
+    return {
+      { "<leader>fh", builtin.help_tags, { desc = "Find Help" } },
+      { "<leader>gr", builtin.live_grep, { desc = "Live Grep" } },
+      { "<leader>gc", builtin.grep_string, { desc = "Grep string under the Cursor" } },
+      { "<leader>ef", builtin.find_files, { desc = "Edit file (telescope)" } },
+      { "<leader>eb", builtin.buffers, { desc = "Edit buffer" } },
+      { "<leader>ec", builtin.commands, { desc = "Execute Vim command" } },
+      { "<leader>er", builtin.oldfiles, { desc = "Edit recent file" } },
 
-    vim.keymap.set("n", "<leader>eh", function()
-      builtin.find_files({
-        hidden = true,
-        file_ignore_patterns = { "^.git/" },
-      })
-    end, { desc = "Edit file (including hidden)" })
+      {
+        "<leader>eh",
+        function()
+          builtin.find_files({
+            hidden = true,
+            file_ignore_patterns = { "^.git/" },
+          })
+        end,
+        { desc = "Edit file (including hidden)" },
+      },
 
-    vim.keymap.set("n", "<leader>en", function()
-      builtin.find_files({
-        cwd = vim.fn.stdpath("config"),
-      })
-    end, { desc = "Edit Neovim config file" })
+      {
+        "<leader>en",
+        function()
+          builtin.find_files({
+            cwd = vim.fn.stdpath("config"),
+          })
+        end,
+        { desc = "Edit Neovim config file" },
+      },
 
-    vim.keymap.set("n", "<leader>eb", function()
-      builtin.buffers()
-    end, { desc = "Edit buffer" })
+      {
+        "<leader>ep",
+        function()
+          builtin.find_files({
+            cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+          })
+        end,
+        { desc = "Edit plugin Neovim file" },
+      },
 
-    vim.keymap.set("n", "<leader>ec", function()
-      builtin.commands()
-    end, { desc = "Execute Vim command" })
-
-    vim.keymap.set("n", "<leader>ep", function()
-      builtin.find_files({
-        cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
-      })
-    end, { desc = "Edit plugin Neovim file" })
-
-    vim.keymap.set("n", "<leader>df", function()
-      builtin.find_files({
-        cwd = "$HOME/dev/0-github-my/dotfiles-personal",
-        hidden = true,
-      })
-    end, { desc = "Edit dotfile" })
-
-    vim.keymap.set("n", "<leader>er", builtin.oldfiles, { desc = "Edit recent file" })
+      {
+        "<leader>df",
+        function()
+          builtin.find_files({
+            cwd = "$HOME/dev/0-github-my/dotfiles-personal",
+            hidden = true,
+          })
+        end,
+        { desc = "Edit dotfile" },
+      },
+    }
   end,
 }
