0f0ac75d5730f5837f8fb984a5646b3b431705e8
Martin Krauskopf <martin.krauskopf@gmail.com>
2025-04-13 09:20:27 +0200

    auto-reformat before touching

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,38 +16,40 @@ return {
   },
 
   config = function()
-    require('lspconfig').lua_ls.setup {}
+    require("lspconfig").lua_ls.setup({})
 
     -- The below mapping is not yet in the stable release. Once it arrives
     -- there, you can delete it.
-    vim.keymap.set('n', 'grn', function()
+    vim.keymap.set("n", "grn", function()
       vim.lsp.buf.rename()
-    end, { desc = 'vim.lsp.buf.rename()' })
+    end, { desc = "vim.lsp.buf.rename()" })
 
-    vim.keymap.set({ 'n', 'x' }, 'gra', function()
+    vim.keymap.set({ "n", "x" }, "gra", function()
       vim.lsp.buf.code_action()
-    end, { desc = 'vim.lsp.buf.code_action()' })
+    end, { desc = "vim.lsp.buf.code_action()" })
 
-    vim.keymap.set('n', 'grr', function()
+    vim.keymap.set("n", "grr", function()
       vim.lsp.buf.references()
-    end, { desc = 'vim.lsp.buf.references()' })
+    end, { desc = "vim.lsp.buf.references()" })
 
-    vim.keymap.set('i', '<C-S>', function()
+    vim.keymap.set("i", "<C-S>", function()
       vim.lsp.buf.signature_help()
-    end, { desc = 'vim.lsp.buf.signature_help()' })
+    end, { desc = "vim.lsp.buf.signature_help()" })
 
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if not client then return end
+        if not client then
+          return
+        end
 
         -- auto-format on save
-        if client.supports_method('textDocument/formatting') then
+        if client.supports_method("textDocument/formatting") then
           vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = args.buf,
             callback = function()
               vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-            end
+            end,
           })
         end
       end,
