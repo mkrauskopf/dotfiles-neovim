2448005060e5d6fc4c4c987a03f0cb8eca30c0ce
Martin Krauskopf <martin.krauskopf@gmail.com>
2025-10-26 17:56:39 +0100

    Migrate auto-session configuration to new version

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,17 +1,16 @@
 return {
   "rmagatti/auto-session",
   lazy = false,
+
   keys = {
-    { '<leader>wr', '<cmd>SessionRestore<CR>', desc = 'Session restore' },
-    { '<leader>wl', '<cmd>SessionSearch<CR>',  desc = 'Session load' },
-    { '<leader>ws', '<cmd>SessionSave<CR>',    desc = 'Save session' },
+    { "<leader>wr", "<cmd>AutoSession restore<CR>", desc = "Session restore" },
+    { "<leader>wl", "<cmd>AutoSession search<CR>", desc = "Session load" },
+    { "<leader>ws", "<cmd>AutoSession save<CR>", desc = "Save session" },
   },
 
   config = function()
-    local auto_session = require("auto-session")
-
-    auto_session.setup({
-      auto_restore_enabled = false,
+    require("auto-session").setup({
+      auto_restore = false,
     })
-  end
+  end,
 }
