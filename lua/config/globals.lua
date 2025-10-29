29d5c9324e30498ff266e10878f432d7890a985a
Martin Krauskopf <martin.krauskopf@gmail.com>
2025-10-29 20:32:10 +0100

    Add `CopyPath` command
    Copy the current buffer path to the OS clipboard.

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -9,3 +9,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
+
+-- Copy the current buffer path to the OS clipboard.
+vim.api.nvim_create_user_command("CopyPath", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  print("Copied: " .. path)
+end, { desc = "Copy the current buffer path to the OS clipboard." })
