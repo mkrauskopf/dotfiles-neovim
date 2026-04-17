b5eb8d9ebfd365a86cf40ca6a650f010d543394b
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-04-17 15:41:46 +0200

    Do not fix the telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,6 @@ local martinovo = require("martinovo")
 
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
