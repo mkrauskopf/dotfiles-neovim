1693fd985306244be42486792a0c6d8eeffed5da
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-04-19 21:06:29 +0200

    Let's try Neogit plugin

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,11 @@
+return {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+  },
+}
