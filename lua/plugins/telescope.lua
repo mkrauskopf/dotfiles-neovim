bd19bb49e9bef4a03110dcd6c51c3a2fbfe8a2fd
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-06-03 14:12:05 +0200

    Telescope: switch to the 'top' layout

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,12 @@ return {
 
   config = function()
     require("telescope").setup({
+      defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+          prompt_position = "top",
+        },
+      },
       extensions = {
         fzf = {},
       },
