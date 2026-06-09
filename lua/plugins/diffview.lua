1011d187e54598c5810f8cde7f795fb39dd0f550
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-06-09 11:05:28 +0200

    gdb -> git diff branch

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,6 +3,7 @@ return {
 
   keys = {
     { "<leader>gdd", ":DiffviewOpen<CR>", desc = "Git diff: focus current file" },
+    { "<leader>gdb", ":DiffviewOpen origin/main..HEAD<CR>", desc = "Git diff: current branch vs origin/main" },
     { "<leader>gdc", ":DiffviewClose<CR>", desc = "Git diff: close" },
   },
 }
