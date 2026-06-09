1b0ca812bd01aa90f9e110fb5a3f2746b23acc7c
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-06-09 11:25:04 +0200

    diffview: register cmd triggers so plugin loads on direct command use

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,6 +1,15 @@
 return {
   "sindrets/diffview.nvim",
 
+  cmd = {
+    "DiffviewOpen",
+    "DiffviewClose",
+    "DiffviewFileHistory",
+    "DiffviewFocusFiles",
+    "DiffviewToggleFiles",
+    "DiffviewRefresh",
+  },
+
   keys = {
     { "<leader>gdd", ":DiffviewOpen<CR>", desc = "Git diff: focus current file" },
     { "<leader>gdb", ":DiffviewOpen origin/main..HEAD<CR>", desc = "Git diff: current branch vs origin/main" },
