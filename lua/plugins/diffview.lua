ee6e06fa6a8e83b4fc8093dc63d21b71bdb5e1ba
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-06-10 12:29:10 +0200

    Ability to review last `n` commits

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -14,5 +14,13 @@ return {
     { "<leader>gdd", ":DiffviewOpen<CR>", desc = "Git diff: focus current file" },
     { "<leader>gdb", ":DiffviewOpen origin/main..HEAD<CR>", desc = "Git diff: current branch vs origin/main" },
     { "<leader>gdc", ":DiffviewClose<CR>", desc = "Git diff: close" },
+    { "<leader>gdl", ":DiffviewLast ", desc = "Git diff: last N commits" },
   },
+
+  init = function()
+    vim.api.nvim_create_user_command("DiffviewLast", function(opts)
+      local n = tonumber(opts.args) or 1
+      vim.cmd(("DiffviewOpen HEAD~%d..HEAD"):format(n))
+    end, { nargs = "?", desc = "Diffview of last N commits (default 1)" })
+  end,
 }
