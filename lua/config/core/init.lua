e5d7beffb300d1bfb8bff5ef32aee74cdd619717
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-07-23 18:25:32 +0200

    recognize Codex rules as starlark filetype

--- lua/config/core/init.lua
+++ lua/config/core/init.lua
@@ -1,3 +1,9 @@
 require("config.core.globals")
 require("config.core.options")
 require("config.core.keymaps")
+
+vim.filetype.add({
+  pattern = {
+    [".*/%.codex/rules/[^/]+%.rules"] = "starlark",
+  },
+})
