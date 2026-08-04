5649752c6cc0f1b82e7148774c4fe37dc0e47354
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-08-04 16:51:47 +0200

    Functionality to compare two files in nvim-tree.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -13,6 +13,27 @@ return {
         vim.keymap.set("n", "<C-Left>", "<cmd>NvimTreeResize -2<CR>", opts)
         vim.keymap.set("n", "<C-Right>", "<cmd>NvimTreeResize +2<CR>", opts)
 
+        -- Functionality to compare two files selected (`m`) in the nvim-tree.
+        local diff_marked = function()
+          local nodes = api.marks.list()
+
+          if #nodes ~= 2 then
+            vim.notify("Mark exactly two files with m", vim.log.levels.WARN)
+            return
+          end
+
+          if nodes[1].type == "directory" or nodes[2].type == "directory" then
+            vim.notify("Both marked nodes must be files", vim.log.levels.WARN)
+            return
+          end
+
+          api.tree.close()
+          vim.cmd("tabedit " .. vim.fn.fnameescape(nodes[1].absolute_path))
+          vim.cmd("vertical diffsplit " .. vim.fn.fnameescape(nodes[2].absolute_path))
+        end
+        vim.keymap.set("n", "<leader>df", diff_marked, { buffer = bufnr, nowait = true, desc = "Diff marked files" })
+        --
+
         local live_grep_in_node_dir = function()
           local node = api.tree.get_node_under_cursor()
           if not node then
