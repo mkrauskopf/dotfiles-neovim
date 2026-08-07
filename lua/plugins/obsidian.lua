6565f5c4e3c7306f52a324a78faa6a6d05182b89
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-08-07 17:02:02 +0200

    Turn on preview by default for Obsidian notes.

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -12,5 +12,13 @@ return {
     require("obsidian").setup(opts)
     vim.keymap.set("n", "<leader>ot", "<cmd>Obsidian tags<CR>", { desc = "Obsidian: tags" })
     vim.keymap.set("n", "<leader>os", "<cmd>Obsidian search<CR>", { desc = "Obsidian: search" })
+
+    -- markview's preview is off by default (see lua/plugins/markview.lua); turn it on for Obsidian notes.
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "ObsidianNoteEnter",
+      callback = function()
+        require("markview").commands.enable(vim.api.nvim_get_current_buf())
+      end,
+    })
   end,
 }
