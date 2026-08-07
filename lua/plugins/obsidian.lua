c32e5018c8b79785110c89619683b1b0bd679a99
Martin Krauskopf <martin.krauskopf@gmail.com>
2026-08-07 16:41:23 +0200

    Initial keymap for Obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -8,4 +8,9 @@ return {
     -- workspaces are machine-specific vault paths; kept out of git, see lua/config/local.lua.example
     workspaces = require("config.local").obsidian_workspaces,
   },
+  config = function(_, opts)
+    require("obsidian").setup(opts)
+    vim.keymap.set("n", "<leader>ot", "<cmd>Obsidian tags<CR>", { desc = "Obsidian: tags" })
+    vim.keymap.set("n", "<leader>os", "<cmd>Obsidian search<CR>", { desc = "Obsidian: search" })
+  end,
 }
